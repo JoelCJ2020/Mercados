ebe2bc7a86bc68ab22fcb381ced7acd318301741
JoelCJ <raybanno2020@gmail.com>
2020-10-02 17:44:22 -0300

    asd

--- src/main/java/cl/mercados/ws/dto/v10/package-info.java
+++ src/main/java/cl/mercados/ws/dto/v10/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2020.10.02 a las 05:34:30 PM CLST 
+// Generado el: 2020.10.02 a las 05:42:25 PM CLST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/mercadito_v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
